81e80a5036e670ffa1e064391d98b6609205a212
zee <zczq912@ucl.ac.uk>
2025-10-24 00:43:53 +0100

    update: v1 - v2

--- v1.ino
+++ v1.ino
@@ -1,4 +1,4 @@
-// Ziyi Wang Oct 2025 - v1 - MQTT messager to vespera with temperature and touch sensors
+// Ziyi Wang Oct 2025 - v2 - MQTT messager to vespera with temperature and touch sensors (switch)
 // Works with MKR1010
 
 #include <SPI.h>
@@ -6,33 +6,36 @@
 #include <PubSubClient.h>
 #include <utility/wifi_drv.h>
 
-#define TEMP_PIN A0   // Temperature sensor
+#define TEMP_PIN A0         // Temperature sensor
+#define TOUCH_PIN A2        // Touch sensor
+bool lightOn = true;        // Record the state
+bool lastTouchState = LOW;  // Detect touch
 
 //Network Configuration
-const char* ssid          = "Glide-US-Resident";
-const char* password      = "ColdAgedCadet";
+const char* ssid = "Glide-US-Resident";
+const char* password = "ColdAgedCadet";
 const char* mqtt_username = "student";
 const char* mqtt_password = "ce2021-mqtt-forget-whale";
-const char* mqtt_server   = "mqtt.cetools.org";
-const int mqtt_port       = 1884;
+const char* mqtt_server = "mqtt.cetools.org";
+const int mqtt_port = 1884;
 
 // create wifi object and mqtt object
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
 // Make sure to update your lightid value below with the one you have been allocated
-String lightId = "27"; // the topic id number or user number being used.
+String lightId = "27";  // the topic id number or user number being used.
 
 // Here we define the MQTT topic we will be publishing data to
-String mqtt_topic = "student/CASA0014/luminaire/" + lightId;            
-String clientId = ""; // will set once i have mac address so that it is unique
+String mqtt_topic = "student/CASA0014/luminaire/" + lightId;
+String clientId = "";  // will set once i have mac address so that it is unique
 
-// NeoPixel Configuration - we need to know this to know how to send messages 
-// to vespera 
+// NeoPixel Configuration - we need to know this to know how to send messages
+// to vespera
 const int num_leds = 72;
-const int payload_size = num_leds * 3; // x3 for RGB
+const int payload_size = num_leds * 3;  // x3 for RGB
 
-// Create the byte array to send in MQTT payload this stores all the colours 
+// Create the byte array to send in MQTT payload this stores all the colours
 // in memory so that they can be accessed in for example the rainbow function
 byte RGBpayload[payload_size];
 
@@ -71,43 +74,63 @@ void loop() {
   if (!mqttClient.connected()) {
     reconnectMQTT();
   }
-  
-  if (WiFi.status() != WL_CONNECTED){
+
+  if (WiFi.status() != WL_CONNECTED) {
     startWifi();
   }
   // keep mqtt alive
   mqttClient.loop();
 
-  int raw = analogRead(TEMP_PIN);               // Read T
-  float voltage = raw * (3.3 / 1023.0);         // Calculate V
-  float temperatureC = (voltage - 0.5) / 0.01;  // Calculate ℃, 1℃ for every 0.01V
+  int touchState = digitalRead(TOUCH_PIN);  // Read touch
 
-  // Print T in monitor
-  Serial.print("Temperature: ");
-  Serial.print(temperatureC);
-  Serial.println(" °C");
-
-  // Map T to colour
-  int r = constrain(map(temperatureC, 20, 35, 0, 255), 0, 255); // T ⬆, r ⬆
-  int b = constrain(map(temperatureC, 20, 35, 255, 0), 0, 255); // T ⬇, b ⬆
-  int g = 50;
-
-  for (int n = 0; n < num_leds; n++) {
-    RGBpayload[n * 3 + 0] = (byte)r;
-    RGBpayload[n * 3 + 1] = (byte)g;
-    RGBpayload[n * 3 + 2] = (byte)b;
+  if (touchState == HIGH && lastTouchState == LOW) {
+    lightOn = !lightOn;  // Switch the state
+    Serial.print("Touch. Light: ");
+    Serial.println(lightOn ? "ON" : "OFF");
+    delay(300);
   }
+  lastTouchState = touchState;  // Record the current state
 
-  if (mqttClient.connected()) {
+  if (lightOn) {
+    int raw = analogRead(TEMP_PIN);               // Read T
+    float voltage = raw * (3.3 / 1023.0);         // Calculate V
+    float temperatureC = (voltage - 0.5) / 0.01;  // Calculate ℃, 1℃ for every 0.01V
+
+    // Print T in monitor
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" °C");
+
+    // Map T to colour
+    int r = constrain(map(temperatureC, 20, 35, 0, 255), 0, 255);  // T ⬆, r ⬆
+    int b = constrain(map(temperatureC, 20, 35, 255, 0), 0, 255);  // T ⬇, b ⬆
+    int g = 50;
+
+    for (int n = 0; n < num_leds; n++) {
+      RGBpayload[n * 3 + 0] = (byte)r;
+      RGBpayload[n * 3 + 1] = (byte)g;
+      RGBpayload[n * 3 + 2] = (byte)b;
+    }
+
+    if (mqttClient.connected()) {
+      mqttClient.publish(mqtt_topic.c_str(), RGBpayload, payload_size);
+      Serial.print("Published color → R:");
+      Serial.print(r);
+      Serial.print(" G:");
+      Serial.print(g);
+      Serial.print(" B:");
+      Serial.println(b);
+    }
+  }
+  else {
+    // Lights off
+    for (int n = 0; n < num_leds; n++) {
+      RGBpayload[n * 3 + 0] = 0;
+      RGBpayload[n * 3 + 1] = 0;
+      RGBpayload[n * 3 + 2] = 0;
+    }
     mqttClient.publish(mqtt_topic.c_str(), RGBpayload, payload_size);
-    Serial.print("Published color → R:");
-    Serial.print(r);
-    Serial.print(" G:");
-    Serial.print(g);
-    Serial.print(" B:");
-    Serial.println(b);
   }
-
   delay(1000);
 }
 
@@ -120,4 +143,3 @@ void printMacAddress(byte mac[]) {
   }
   Serial.println();
 }
-
